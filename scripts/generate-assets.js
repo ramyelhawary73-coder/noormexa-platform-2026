fb9542ad2f639169d93679abcad9accd7f8a48b8
ramyelhawary73-coder <ramyelhawary73@gmail.com>
2026-08-21 23:44:11 +0300

    build: remove redundant app directory icons
    Clean up the asset generation script by removing the 
    unnecessary icon file creation in the app directory.

--- scripts/generate-assets.js
+++ scripts/generate-assets.js
@@ -167,8 +167,6 @@ async function run() {
   console.log('Generating 96x96 and 32x32 Favicons...');
   await sharp(svgBuffer).resize(96, 96).png().toFile(path.join(publicDir, 'favicon.png'));
   await sharp(svgBuffer).resize(32, 32).png().toFile(path.join(publicDir, 'favicon-32.png'));
-  await sharp(svgBuffer).resize(32, 32).png().toFile(path.join(appDir, 'icon.png'));
-  await sharp(svgBuffer).resize(180, 180).png().toFile(path.join(appDir, 'apple-icon.png'));
 
   console.log('Assets successfully generated!');
 }
